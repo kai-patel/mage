a0541c0a72da0110d60385deccdedf927e5df90d
Kai Patel <00kaipatel@gmail.com>
2023-06-07 13:33:50 +0100

    feat(lexer): add interface

---
+++ src/lexer.h
@@ -0,0 +1,43 @@
+#ifndef LEXER_H_
+#define LEXER_H_
+#include <stdint.h>
+typedef enum {
+  Token_Illegal,
+  Token_Eof,
+  Token_Ident,
+  Token_Int,
+  Token_Assign,
+  Token_Plus,
+  Token_Minus,
+  Token_Bang,
+  Token_Asterisk,
+  Token_Slash,
+  Token_LT,
+  Token_GT,
+  Token_Equal,
+  Token_NotEqual,
+  Token_Comma,
+  Token_Semicolon,
+  Token_LParen,
+  Token_RParen,
+  Token_LCurly,
+  Token_RCurly,
+  Token_Function,
+  Token_Let,
+  Token_True,
+  Token_False,
+  Token_If,
+  Token_Else,
+  Token_Return,
+} TokenType;
+
+typedef struct {
+  TokenType type;
+  char *lexeme;
+} Token;
+
+typedef struct LexerStruct Lexer;
+
+Lexer *lexer_new(const char *);
+Token *lexer_next(Lexer *);
+#endif
