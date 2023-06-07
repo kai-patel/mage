1f8733ac322f6df09b72cbd7de6a359e0166e306
Kai Patel <00kaipatel@gmail.com>
2023-06-07 20:19:42 +0100

    feat(lexer): support alphanum + underscore idents

--- src/lexer.c
+++ src/lexer.c
@@ -1,8 +1,10 @@
 #include <assert.h>
 #include <ctype.h>
+#include <errno.h>
 #include <lexer.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct LexerStruct {
   const char *input;
@@ -29,6 +31,59 @@ static void step(Lexer *lexer) { lexer->ch = lexer->input[lexer->position++]; }
 
 static char peek(Lexer *lexer) { return lexer->input[lexer->position]; }
 
+static void ignore_whitespace(Lexer *lexer) {
+  while (isblank(lexer->ch)) {
+    step(lexer);
+  }
+  return;
+}
+
+static size_t read_identifier_length(Lexer *lexer) {
+  size_t start_pos = lexer->position;
+
+  while (isalnum(lexer->ch) || lexer->ch == '_') {
+    step(lexer);
+  }
+
+  return lexer->position - start_pos;
+}
+
+static TokenType identifier_to_type(const char *identifier) {
+  if (strcmp(identifier, "defn") == 0) {
+    return Token_Function;
+  }
+  if (strcmp(identifier, "let") == 0) {
+    return Token_Let;
+  }
+  if (strcmp(identifier, "true") == 0) {
+    return Token_True;
+  }
+  if (strcmp(identifier, "false") == 0) {
+    return Token_False;
+  }
+  if (strcmp(identifier, "if") == 0) {
+    return Token_If;
+  }
+  if (strcmp(identifier, "else") == 0) {
+    return Token_Else;
+  }
+  if (strcmp(identifier, "while") == 0) {
+    return Token_While;
+  }
+  if (strcmp(identifier, "return") == 0) {
+    return Token_Return;
+  }
+
+  return Token_Ident;
+}
+
+static char *identifier_to_lexeme(const char *identifier, TokenType t) {
+  if (t == Token_Ident) {
+    return (char *)identifier;
+  }
+  return NULL;
+}
+
 static Token *make_token(TokenType t, char *lexeme) {
   Token *token = malloc(sizeof(*token));
 
@@ -47,6 +102,7 @@ Token *lexer_next(Lexer *lexer) {
   step(lexer);
   Token *token = NULL;
 
+  ignore_whitespace(lexer);
   char c = lexer->ch;
 
   // Single character tokens
@@ -106,6 +162,32 @@ Token *lexer_next(Lexer *lexer) {
   case '\0':
     token = make_token(Token_Eof, NULL);
     break;
+  }
+
+  // String Tokens
+  if (isalpha(c) || c == '_') {
+    size_t len = read_identifier_length(lexer);
+    char *identifier = malloc(sizeof(*identifier) * len + 1);
+
+    if (identifier == NULL) {
+      perror("Could not create identifier");
+      exit(1);
+    }
+
+    errno_t result =
+        strncpy_s(identifier, sizeof(char) * len + 1, lexer->input, len);
+
+    if (result != 0) {
+      perror("Could not copy identifier string");
+      exit(1);
+    }
+
+    TokenType id_type = identifier_to_type(identifier);
+    char *id_lexeme = identifier_to_lexeme(identifier, id_type);
+
+    token = make_token(id_type, id_lexeme);
+
+    return token;
   }
 
   if (token == NULL) {
