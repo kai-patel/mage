2325d751f790be4edf18afd799aac460ae130ad0
Kai Patel <00kaipatel@gmail.com>
2023-06-07 15:14:33 +0100

    feat: add lexer to mage.lib

--- include/mage.h
+++ include/mage.h
@@ -1,7 +1,6 @@
 #ifndef MAGE_H_
 #define MAGE_H_
-#include <stdint.h>
 
-uint32_t addThenMultiply(uint32_t, uint32_t, uint32_t);
+#include "lexer.h"
 
 #endif
--- src/mage.c
+++ src/mage.c
@@ -1,15 +1,7 @@
 #include "mage.h"
+#include "lexer.h"
 #include <stdint.h>
 #include <stdio.h>
 
-uint32_t addThenMultiply(uint32_t a, uint32_t b, uint32_t c) {
-  uint32_t addition = a + b;
-  return addition * c;
-}
-
 int main() {
-  uint32_t result = addThenMultiply(5, 3, 2);
-  printf("%d\n", result);
-
-  return 0;
 }
