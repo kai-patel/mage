d36c053ddf9f6bc26df7176a77f56127bff8bcdf
Kai Patel <00kaipatel@gmail.com>
2023-06-07 15:14:53 +0100

    test(lexer): add test for lexing single tokens

---
+++ tests/test_lexer.cpp
@@ -0,0 +1,37 @@
+#include "lexer.h"
+#include "mage.h"
+#include <gtest/gtest.h>
+
+TEST(LexerTests, SingleTokens) {
+  const char *input = "+-*/!!====,;{()}";
+  Token expected[] = {
+      {Token_Plus, NULL},      {Token_Minus, NULL},  {Token_Asterisk, NULL},
+      {Token_Slash, NULL},     {Token_Bang, NULL},   {Token_NotEqual, NULL},
+      {Token_Equal, NULL},     {Token_Assign, NULL}, {Token_Comma, NULL},
+      {Token_Semicolon, NULL}, {Token_LCurly, NULL}, {Token_LParen, NULL},
+      {Token_RParen, NULL},    {Token_RCurly, NULL}, {Token_Eof, NULL}};
+
+  size_t n_expected = 15;
+
+  Token *actual[n_expected];
+
+  Lexer *lexer = lexer_new(input);
+  Token *token = NULL;
+  size_t n_actual = 0;
+
+  while (token->type != Token_Eof) {
+    token = lexer_next(lexer);
+    actual[n_actual++] = token;
+  }
+
+  ASSERT_EQ(n_actual, n_expected - 1);
+
+  for (size_t i = 0; i < n_expected; i++) {
+    EXPECT_EQ(expected[i].type, actual[i]->type);
+    EXPECT_EQ(expected[i].lexeme, actual[i]->lexeme);
+  }
+
+  for (size_t i = 0; i < n_expected; i++) {
+    free(actual[i]);
+  }
+}
